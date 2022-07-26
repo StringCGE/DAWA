d1960914036336fab256d4b171c2209bf4a1c709
Iván Espinoza de los Monteros <ivanespinoza2499@gmail.com>
2022-07-25 20:31:05 -0500

    Últimos cambios

--- Clases/Tarea1/src/app/api.service.ts
+++ Clases/Tarea1/src/app/api.service.ts
@@ -16,16 +16,16 @@ export class ApiService {
   constructor() { }
   protected lItemRopaCarrito:ItemRopaCarrito[] = [];
   protected lItemRopa:ItemRopa[] = [
-    new ItemRopa(1,"Gracias",29.00,13,"../../assets/img/seccion-hombre/item_01.jpg",13),
-    new ItemRopa(2,"Roopa",35.50,22,"../../assets/img/seccion-hombre/item_02.jpg",22),
-    new ItemRopa(3,"Roopa",59.99,145,"../../assets/img/seccion-hombre/item_03.jpg",145),
-    new ItemRopa(4,"Roopa",45.00,48,"../../assets/img/seccion-hombre/item_04.jpg",48),
-    new ItemRopa(5,"Roopa",35.99,74,"../../assets/img/seccion-hombre/item_05.jpg",74),
-    new ItemRopa(6,"Roopa",25.99,62,"../../assets/img/seccion-hombre/item_06.jpg",62),
-    new ItemRopa(7,"Roopa",15.99,50,"../../assets/img/seccion-hombre/item_07.jpg",50),
-    new ItemRopa(8,"Roopa",5.99,70,"../../assets/img/seccion-hombre/item_08.jpg",70),
-    new ItemRopa(9,"Roopa",0.99,28,"../../assets/img/seccion-hombre/item_09.jpg",28),
-    new ItemRopa(10,"Roopa",0.09,80,"../../assets/img/seccion-hombre/item_10.jpg",80)
+    new ItemRopa(1,"Alpha Industries Chaqueta",29.00,13,"../../assets/img/seccion-hombre/item_01.jpg",13),
+    new ItemRopa(2,"Chaqueta Vaquera Slim",35.50,22,"../../assets/img/seccion-hombre/item_02.jpg",22),
+    new ItemRopa(3,"Chaqueta de cuero para hombre",59.99,145,"../../assets/img/seccion-hombre/item_03.jpg",145),
+    new ItemRopa(4,"Camiseta deportiva Vans",45.00,48,"../../assets/img/seccion-hombre/item_04.jpg",48),
+    new ItemRopa(5,"Buzo deportiva para caballero",35.99,74,"../../assets/img/seccion-hombre/item_05.jpg",74),
+    new ItemRopa(6,"Camiseta Jogger para hombres",25.99,62,"../../assets/img/seccion-hombre/item_06.jpg",62),
+    new ItemRopa(7,"Chaqueta Street Style",15.99,50,"../../assets/img/seccion-hombre/item_07.jpg",50),
+    new ItemRopa(8,"Camiseta Vans Casual para hombre",5.99,70,"../../assets/img/seccion-hombre/item_08.jpg",70),
+    new ItemRopa(9,"Jeans Vaqueros",0.99,28,"../../assets/img/seccion-hombre/item_09.jpg",28),
+    new ItemRopa(10,"Next Level camisa mangas cortas",0.09,80,"../../assets/img/seccion-hombre/item_10.jpg",80)
   ];
   protected lItemRopaMujer:ItemRopa[] = [
     new ItemRopa(6001,"Calentador",23.00,20,"../../assets/img/seccion-mujer/item_01.png",13),
